9320cbd247fd19951d18394f29c0b97eab9d90d1
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-03-17 12:56:10 +0530

    update

--- DSAquestion.js
+++ DSAquestion.js
@@ -1,47 +1,47 @@
 // Q.4 Given an array, make an object with key of that specific index wouth value from array
 
-var array = [34, 45, 567, 78, 89, 34, 67, 87, 34, 78, 89]
+// var array = [34, 45, 567, 78, 89, 34, 67, 87, 34, 78, 89]
 
-var result = {}
+// var result = {}
 
-for (var i = 0; i < array.length; i++) {
-    result[i] = (array[i]);
-}
-console.log(result, "result");
+// for (var i = 0; i < array.length; i++) {
+//     result[i] = (array[i]);
+// }
+// console.log(result, "result");
 
 
 // Q.3 find whether the given number is present in the given array or Notification. if found print index if not then print "not found"
 
-var number = 19;
-var array = [3, 5, 79, 45, 0, -67, 23, 77, 19, 78, 12];
+// var number = 19;
+// var array = [3, 5, 79, 45, 0, -67, 23, 77, 19, 78, 12];
 
-flag = false
+// flag = false
 
-for (var i = 0; i < array.length; i++) {
-    if (number = array.length) {
-        flag = true;
-    }
-}
+// for (var i = 0; i < array.length; i++) {
+//     if (number = array.length) {
+//         flag = true;
+//     }
+// }
 
-if (flag = true) {
-    console.log("number is found");
-}
-else {
-    console.log("number not found");
-}
+// if (flag = true) {
+//     console.log("number is found");
+// }
+// else {
+//     console.log("number not found");
+// }
 
 
 // Q.2 find the addition of every second number from the given range
 
-var start = 5768;
-var end = 78656;
+// var start = 5768;
+// var end = 78656;
 
-count = [];
+// count = [];
 
-for (var i = 5768; i < 78656; i++) {
+// for (var i = 5768; i < 78656; i++) {
 
 
-}
+// }
 
 // Q.1 find the addition of number which match the given target from the given array. if found then print the number if not then "not found"
 
@@ -51,8 +51,10 @@ var target = 66;
 
 for (var i = 0; i < array.length; i++) {
     // console.log(array);
-    for (var i = 0; j < array.length; j++) {
-        target = i + j;
+    for (var j=i; j < array.length; j++) {
+       if(array[i] + array[j] == target){
+        console.log(array[i], array[j])
+       }
     }
 
 }
